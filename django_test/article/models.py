4b54420654ee75f4ef91229ed5e97434950c5f6d
satish <satishmtse18@gmail.com>
2015-01-16 16:20:52 +0530

    minor change

--- django_test/article/models.py
+++ django_test/article/models.py
@@ -9,6 +9,7 @@ class Article(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     pub_date = models.DateField(default=date.today())
+    
     likes = models.IntegerField()
 
     def __str__(self):
