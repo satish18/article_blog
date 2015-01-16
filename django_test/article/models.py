6ff4022e04d00cbfb9dd4e19387c1fac13c4dbc5
satish <satishmtse18@gmail.com>
2015-01-16 16:09:21 +0530

    model added

--- django_test/article/models.py
+++ django_test/article/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+class Article(models.Model):
+    """
+
+    """
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    pub_date = models.DateField(default=date.today())
+    likes = models.IntegerField()
+
+    def __str__(self):
+        return self.title
